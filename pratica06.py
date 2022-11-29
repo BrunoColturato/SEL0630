8d212e7ce6d5643e7a4f1b3202973bf51bbec278
Guerreiro51 <alessandrog54@gmail.com>
2022-11-29 20:37:18 -0300

    Implemented camera funcionality. TODO: Implement video

--- pratica06.py
+++ pratica06.py
@@ -10,4 +10,23 @@ temperatura = weather[0]["ambient_temp"]
 data = weather[0]["created_on"]
 created = weather[0]["created_by"]
 
-pprint(temperatura, data, created)
+# Camera da Rasp
+camera = PiCamera()
+
+# Configuracoes da camera
+camera.resolution = (1920, 1080)
+camera.framerate = 15
+
+# Adicionando efeito
+effect = 'cartoon'
+camera.image_effect = effect
+
+# Adicionando texto
+camera.annotate_background = Color('black')
+camera.annotate_text = f"Bruno 11200251 Alessandro 11233891 \nTemperatura: {temperatura} \nData: {data} \nCriado por: {created}"
+
+# Tirando foto com efeito e texto
+camera.start_preview()
+sleep(5)
+camera.capture('/home/sel/workab/bruno_alessandrox.jpg')
+camera.stop_preview()
